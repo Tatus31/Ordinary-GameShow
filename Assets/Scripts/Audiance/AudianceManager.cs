f3c7c17b1b271d4a2b1969df3f5c264ec33e74da
Tatus31 <kris.zborowski2003@gmail.com>
2025-11-09 14:53:34 +0100

    audaince cheering finishing touches

--- Assets/Scripts/Audiance/AudianceManager.cs
+++ Assets/Scripts/Audiance/AudianceManager.cs
@@ -1,15 +1,26 @@
 using System;
 using UnityEngine;
+using System.Collections;
 
 public class AudianceManager : MonoBehaviour
 {
     [SerializeField] private GameObject[] audiance;
+    [Space(10)] 
+    [SerializeField] private Animator audianceAnimator;
+    [SerializeField] private float defaultAnimationTime;
     [Header("Audiance sprites")]
     [SerializeField] private Sprite audianceNeutralSprite; 
     [SerializeField] private Sprite audianceCheerSprite;
     [SerializeField] private Sprite audianceBooSprite;
     
     private SpriteRenderer[] _audianceSprites;
+    
+    private Coroutine _audianceAnimationCoroutine;
+    
+    private static readonly int IsCheer = Animator.StringToHash("IsCheer");
+    private static readonly int IsNeutral = Animator.StringToHash("IsNeutral");
+    private static readonly int IsBoo = Animator.StringToHash("IsBoo");
+
     private void Awake()
     {
         _audianceSprites = new  SpriteRenderer[audiance.Length];
@@ -20,19 +31,69 @@ public class AudianceManager : MonoBehaviour
         }
     }
 
-    public void SetAudianceToNeutral()
+    private void Start()
+    {
+        if (!audianceAnimator)
+        {
+#if UNITY_EDITOR
+            Debug.LogWarning("There is not audiance animator");
+#endif
+        }
+        else
+        {
+            audianceAnimator = transform.GetComponent<Animator>();
+        }
+    }
+
+    public void SetAudianceToNeutral(float duration)
     {
         ChangeAllSprites(audianceNeutralSprite);
+        
+        if(duration == 0)
+            return;
+        
+        audianceAnimator.SetBool(IsNeutral, true);
+        ChangeAudianceAnimation(IsNeutral, false, duration);
     }
 
-    public void SetAudianceToCheer()
+    public void SetAudianceToCheer(float duration)
     {
         ChangeAllSprites(audianceCheerSprite);
+        
+        if(duration == 0)
+            return;
+        
+        audianceAnimator.SetBool(IsCheer, true);
+        ChangeAudianceAnimation(IsCheer, false, duration);
     }
 
-    public void SetAudianceToBoo()
+    public void SetAudianceToBoo(float duration)
     {
         ChangeAllSprites(audianceBooSprite);
+        
+        if(duration == 0)
+            return;
+        
+        audianceAnimator.SetBool(IsBoo, true);
+        ChangeAudianceAnimation(IsBoo, false, duration);
+    }
+
+    private void ChangeAudianceAnimation(int animationHash, bool animationState, float animationTime = 1f)
+    {
+        if (_audianceAnimationCoroutine != null)
+        {
+            StopCoroutine(_audianceAnimationCoroutine);
+            _audianceAnimationCoroutine = null;
+        }
+        
+        _audianceAnimationCoroutine = StartCoroutine(ChangeAudianceAnimationCoroutine(animationTime,  animationHash, animationState));
+    }
+    
+    private IEnumerator ChangeAudianceAnimationCoroutine(float animationTime, int animationHash, bool animationState)
+    {
+        yield return new WaitForSeconds(animationTime);
+        
+        audianceAnimator.SetBool(animationHash, animationState);
     }
 
     private void ChangeAllSprites(Sprite sprite)
@@ -44,5 +105,8 @@ public class AudianceManager : MonoBehaviour
                 spriteRenderer.sprite = sprite;
             }
         }
+#if UNITY_EDITOR
+        Debug.Log($"changed: {_audianceSprites.Length + 1} to {sprite}");
+#endif
     }
 }
